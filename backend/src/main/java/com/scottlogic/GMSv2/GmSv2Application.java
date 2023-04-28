0c9353dc4474b7a0f353efdfdbef007a93ab258b
Daniel Lucas (He/Him) <dalucas@scottlogic.com>
2023-04-28 11:35:44 +0100

    GMSv2 - SpringBootBackend init

---
+++ backend/src/main/java/com/scottlogic/GMSv2/GmSv2Application.java
@@ -0,0 +1,13 @@
+package com.scottlogic.GMSv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GmSv2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GmSv2Application.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/scottlogic/GMSv2/GmSv2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scottlogic.GMSv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GmSv2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
