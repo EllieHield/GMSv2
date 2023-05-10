9562125ae49058f597566f6e3479f3a3af8e19fe
Matthew Smith <masmith@scottlogic.com>
2023-05-10 12:22:33 +0100

    chore: use nicer fake club data

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -1,7 +1,7 @@
-INSERT INTO club (id, name) VALUES (1, 'Club 1');
-INSERT INTO club (id, name) VALUES (2, 'Club 2');
-INSERT INTO club (id, name) VALUES (3, 'Club 3');
-INSERT INTO club (id, name) VALUES (4, 'Club 4');
+INSERT INTO club (id, name, address, shortName) VALUES (1, 'Firebrands', 'Longwood, Clevedon Road, Failand, Bristol BS8 3TL', 'FHC');
+INSERT INTO club (id, name, address, shortName) VALUES (2, 'Bristol City', 'Ashton Rd, Bristol BS3 2EJ', 'BCFC');
+INSERT INTO club (id, name, address, shortName) VALUES (3, 'Liverpool Football Club', 'Ashton Rd, Bristol BS3 2EJ', 'LFC');
+INSERT INTO club (id, name, address, shortName) VALUES (4, 'Beeston Hockey Club', 'Highfields Park, University Blvd, Nottingham NG7 2PS', 'BHC');
 INSERT INTO club (id, name) VALUES (5, 'Club 5');
 INSERT INTO team (id, name, clubId) VALUES (1, 'Team Amazing', 1);
 INSERT INTO team (id, name, clubId) VALUES (2, 'Matt Team 1', 1);
