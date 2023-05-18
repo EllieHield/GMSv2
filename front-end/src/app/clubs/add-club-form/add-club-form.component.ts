7a12ef447e9251c56792a15da3d3fd329e19b566
dalucas-scottlogic <dalucas@scottlogic.com>
2023-05-18 15:52:11 +0100

    Added validation to form

--- front-end/src/app/clubs/add-club-form/add-club-form.component.ts
+++ front-end/src/app/clubs/add-club-form/add-club-form.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { Club } from 'src/app/types/Club';
 import { ClubService } from 'src/app/club-service/club.service';
@@ -12,20 +12,38 @@ import { ClubService } from 'src/app/club-service/club.service';
 export class AddClubFormComponent {
   constructor(private router: Router, private clubService: ClubService) {}
   clubform = new FormGroup({
-    name: new FormControl(''),
-    shortName: new FormControl(''),
-    address: new FormControl(''),
+    name: new FormControl('', [Validators.required]),
+    shortName: new FormControl('', [
+      Validators.required,
+      Validators.minLength(2),
+      Validators.maxLength(3),
+    ]),
+    address: new FormControl('', [Validators.required]),
   });
 
   submit() {
-    let newClub: Club = {
-      id: '',
-      name: this.clubform.value.name!,
-      address: this.clubform.value.address!,
-      shortName: this.clubform.value.shortName!,
-    };
-    this.clubService
-      .addClub(newClub)
-      .subscribe((_) => this.router.navigate(['clubs']));
+    if (this.clubform.valid) {
+      let newClub: Club = {
+        id: '',
+        name: this.clubform.value.name!,
+        address: this.clubform.value.address!,
+        shortName: this.clubform.value.shortName!,
+      };
+      this.clubService
+        .addClub(newClub)
+        .subscribe((_) => this.router.navigate(['clubs']));
+    }
+  }
+
+  get name() {
+    return this.clubform.get('name');
+  }
+
+  get shortName() {
+    return this.clubform.get('shortName');
+  }
+
+  get address() {
+    return this.clubform.get('address');
   }
 }
